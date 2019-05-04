fb97e35c50aa5037dd30cf2eb47d7710cacbf4cd
tmoynandy <tmoy.nandy@gmail.com>
2019-05-04 18:07:37 +0530

    fetching headline from url

--- fakeNewsDetector/views.py
+++ fakeNewsDetector/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from .webscraper import webscraper
+
 
 # Create your views here.
 # def home(request):
@@ -8,16 +10,26 @@ from django.views.generic import TemplateView
 class HomeView(TemplateView):
     template_name = "fakeNewsDetector/home.html"
 
-    def get_context_data(self, **kwargs):
-        context = super(HomeView, self).get_context_data(**kwargs)
-        return context
+    # def get_context_data(self, **kwargs):
+    #     context = super(HomeView, self).get_context_data(**kwargs)
+    #     return context
 
     def get(self, request, *args, **kwargs):
         q = request.GET.get('q')
         error = ''
         if not q:
             error = "error message"
-        print(q)
-        print(type(q))
-        return render(request, self.template_name, {'error': error})
-
+        else:
+            print(q)
+            print(type(q))
+            # print(webscraper)
+            # print(dir(webscraper))
+            # print(webscraper.headlinescraper)
+            #print(webscraper.headlinescraper('aaaaa'))
+            print(webscraper.headlinescraper(q))
+            headline = webscraper.headlinescraper(q)
+            
+        return render(request, self.template_name, {
+            'error': error,
+            'headline': headline,
+        })
--- fakeNewsDetector/webscraper/webscraper.py
+++ fakeNewsDetector/webscraper/webscraper.py
@@ -1,9 +1,11 @@
 import requests
 import bs4
 
+
 def headlinescraper(url):
+    # if not url.startswith('https://') or not url.startswith('http://'):
+    #     url = 'https://{}'.format(url)
     res = requests.get(url)
     soup = bs4.BeautifulSoup(res.text, 'lxml')
     headline = soup.select('h1')
     return headline[0].getText()
-    
