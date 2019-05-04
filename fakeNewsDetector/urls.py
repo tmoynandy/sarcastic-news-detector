4b2c179ec5e3e634fa35c87c8656aa779f86ccfc
tmoynandy <tmoy.nandy@gmail.com>
2019-05-04 17:10:42 +0530

    added class view which works

--- fakeNewsDetector/urls.py
+++ fakeNewsDetector/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
+#from django.views.generic import TemplateView
+from fakeNewsDetector.views import HomeView
 
 urlpatterns = [
-    path('', views.home, name="home"),
+    # path('', views.home, name="home"),
+    path('', HomeView.as_view(), name="home"),
 ]
--- fakeNewsDetector/views.py
+++ fakeNewsDetector/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
 # Create your views here.
-def home(request):
-    return render(request, 'fakeNewsDetector/home.html', {'title' : 'Home'})
+# def home(request):
+#     return render(request, 'fakeNewsDetector/home.html', {'title' : 'Home'})
+
+class HomeView(TemplateView):
+    template_name = "fakeNewsDetector/home.html"
